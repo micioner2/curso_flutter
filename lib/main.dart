e51c399ebeee9a78a2c054c6495a9a65c8101347
micioner2 <mauriciozpdeveloper@gmail.com>
2023-11-14 21:36:37 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/presentation/screens/counter/counter_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+      
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme:  ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue
+      ),
+      home:const CounterScreen()
+    );
+    
+  }
+}
